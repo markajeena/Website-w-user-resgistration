3aeab6a1533cbfb7daec86b1de8b0c9699898179
markajeena <55476784+markajeena@users.noreply.github.com>
2022-03-13 23:33:05 -0700

    Update and rename error.php to errors.php

--- comp440/error.php
+++
@@ -1,9 +0,0 @@
-<?php if(count($errors) > 0): ?>
-    <div>
-        <?php foreach($errors as $errors) : ?>
-
-            <p><?php echo $errors ?></p>
-
-            <?php endforeach ?>
-        </div>
-        <?php endif ?>
