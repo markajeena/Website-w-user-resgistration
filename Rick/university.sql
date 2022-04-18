04879d4b3c97385e2237646398c1014dd7b00611
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2022-04-18 16:00:52 -0700

    Updated file

--- Rick/university.sql
+++ Rick/university.sql
@@ -1,69 +1,70 @@
 -- Before execute the file, ADD your database name here:
 -- The database name should be the same as your database of your user table from step 1
-use `user_registration`; 
+use `phase1`;
 
 --
 -- Table structure for table `department`
 --
 DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `tag`;
 DROP TABLE IF EXISTS `blog`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`(
+  `userid` bigint(20) COLLATE utf8mb4_general_ci NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `firstname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`username`));
+  PRIMARY KEY (`userid`));
 --
 -- Dumping data for table `user`
 --
-INSERT INTO `user` VALUES ('comp440','pass1234', 'Mark', 'Ajina', 'markajina@sexyman.org'),('comp441','pass1234', 'Ricardo', 'Carrillo', 'bearbear@copcop.com'),('comp442','pass1234','Jesus', 'Garcia', 'Iran@ofideas.com'),('comp443','pass1234', 'Erick', 'Anderson', 'Admiral@aol.com'),('comp444','pass1234','Joe','Bunni','Jojo@Bizzare.com');
+INSERT INTO `user` VALUES ('1','comp440','pass1234', 'Mark', 'Ajina', 'markajina@sexyman.org'),('2','comp440','pass1234', 'Ricardo', 'Carrillo', 'bearbear@copcop.com'),('3','comp442','pass1234','Jesus', 'Garcia', 'Iran@ofideas.com'),('4','comp443','pass1234', 'Erick', 'Anderson', 'Admiral@aol.com'),('5','comp444','pass1234','Joe','Bunni','Jojo@Bizzare.com');
 
 --
 -- Table structure for table `blog`
 --
 
 CREATE TABLE `blog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `subject` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` LONGTEXT COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `blog_date` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `username` (`username`),
-  CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL
+  `blogid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` bigint(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`blogid`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userid`) ON DELETE SET NULL
 );
 --
 -- Dumping data for table `blog`
 --
-INSERT INTO `blog` VALUES ('101','Welcome','Welcome to the blog','comp440', '2022-04-18 12:16:54'),('105','Nice to meet you all','I hope we can all get along','comp440', '2022-04-18 12:16:54'),('123','Boring','Not much to do on this blog','comp440', '2022-04-18 12:16:54'),('127','Having a great time','Posting on this blog is fun','comp440', '2022-04-18 12:16:54'),('130','Leaving this blog','Im leaving for another blog','comp440', '2022-04-18 12:16:54');
+INSERT INTO `blog` VALUES ('101','Welcome','Welcome to the blog','1'),('105','Nice to meet you all','I hope we can all get along','1'),('123','Boring','Not much to do on this blog','2'),('127','Having a great time','Posting on this blog is fun','3'),('130','Leaving this blog','Im leaving for another blog','1');
 
---
--- Table structure for table `comment`
---
 CREATE TABLE `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `comment` MEDIUMTEXT COLLATE utf8mb4_general_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `comment_date` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `username` (`username`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL
+  `commentid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `blog_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`commentid`),
+  KEY `blog_id` (`blog_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`blogid`) ON DELETE SET NULL,
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userid`) ON DELETE SET NULL
+
 );
-INSERT INTO `comment` VALUES ('101','Welcome to the blog','comp440', '2022-04-18 12:16:54'),('105','I hope we can all get along','comp442', '2022-04-18 12:16:54'),('123','Not much to do on this blog','comp441', '2022-04-18 12:16:54'),('127','Posting on this blog is fun','comp443', '2022-04-18 12:16:54'),('130','Im leaving for another blog','comp440', '2022-04-18 12:16:54');
+INSERT INTO `comment` VALUES ('101','Welcome to the blog','101','1'),('105','I hope we can all get along','101','2'),('123','Not much to do on this blog','101','3'),('127','Posting on this blog is fun','101' ,'3'),('130','Im leaving for another blog','101','1');
 
 
 --
--- Table structure for table "tags"
+-- Table structure for table "tag"
 --
-DROP TABLE IF EXISTS `tags`;
-CREATE TABLE `tags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tag` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+  `tagid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`tagid`)
 );
 --
--- Dumping data for table `tags`
+-- Dumping data for table `tag`
 --
-INSERT INTO `tags` VALUES ('14365','NewBlog'),('15347','Friendly'),('19368','Bored'),('22591','Fun, GreatTime'),('25946','Leavinglol');
+INSERT INTO `tag` VALUES ('14365','NewBlog'),('15347','Friendly'),('19368','Bored'),('22591','Fun, GreatTime'),('25946','Leavinglol');
