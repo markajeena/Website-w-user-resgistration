a8d8c5a420c24d654981a73f51fc28b2d7a85f2b
jgarcia11299 <64043191+jgarcia11299@users.noreply.github.com>
2022-04-18 03:03:07 -0700

    Initialize Database Button Working

--- university.sql
+++ university.sql
@@ -5,56 +5,51 @@ use `user_registration`;
 --
 -- Table structure for table `department`
 --
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `blog`;
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`(
   `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `password1` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `firstname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`username`),
-);
+  PRIMARY KEY (`username`));
 --
 -- Dumping data for table `user`
 --
-INSERT INTO `user` VALUES ('comp440','pass1234', 'Mark', 'Ajina', 'markajina@sexyman.org'),('comp440','pass1234', 'Ricardo', 'Carrillo', 'bearbear@copcop.com'),('comp440','pass1234','Jesus', 'Garcia', 'Iran@ofideas.com'),('comp440','pass1234', 'Erick', 'Anderson', 'Admiral@aol.com'),('comp440','pass1234','Joe','Bunni','Jojo@Bizzare.com');
+INSERT INTO `user` VALUES ('comp440','pass1234', 'Mark', 'Ajina', 'markajina@sexyman.org'),('comp441','pass1234', 'Ricardo', 'Carrillo', 'bearbear@copcop.com'),('comp442','pass1234','Jesus', 'Garcia', 'Iran@ofideas.com'),('comp443','pass1234', 'Erick', 'Anderson', 'Admiral@aol.com'),('comp444','pass1234','Joe','Bunni','Jojo@Bizzare.com');
 
 --
 -- Table structure for table `blog`
 --
-DROP TABLE IF EXISTS `blog`;
+
 CREATE TABLE `blog` (
-  `blogid` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `subject` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `description` LONGTEXT COLLATE utf8mb4_general_ci NOT NULL,
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  --CONSTRAINT `course_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL,
+  KEY `username` (`username`),
+  CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL
 );
 --
 -- Dumping data for table `blog`
 --
-INSERT INTO `blog` VALUES ('101','Welcome','Welcome to the blog'),('105','Nice to meet you all','I hope we can all get along'),('123','Boring','Not much to do on this blog'),('127','Having a great time','Posting on this blog is fun'),('130','Leaving this blog','Im leaving for another blog');
+INSERT INTO `blog` VALUES ('101','Welcome','Welcome to the blog','comp440'),('105','Nice to meet you all','I hope we can all get along','comp440'),('123','Boring','Not much to do on this blog','comp440'),('127','Having a great time','Posting on this blog is fun','comp440'),('130','Leaving this blog','Im leaving for another blog','comp440');
 
 --
 -- Table structure for table `comment`
 --
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
-  `commentid` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `comment` MEDIUMTEXT COLLATE utf8mb4_general_ci NOT NULL,
-  `blogid` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  --CONSTRAINT `instructor_ibfk_1` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`) ON DELETE SET NULL,
-  --CONSTRAINT `instructor_chk_1` CHECK ((`salary` > 29000))
+  KEY `username` (`username`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL
 );
---
---Dumping data for table `comment`
---
-INSERT INTO `blog` VALUES ('101','Welcome','Welcome to the blog'),('105','Nice to meet you all','I hope we can all get along'),('123','Boring','Not much to do on this blog'),('127','Having a great time','Posting on this blog is fun'),('130','Leaving this blog','Im leaving for another blog');
+INSERT INTO `comment` VALUES ('101','Welcome to the blog','comp440'),('105','I hope we can all get along','comp442'),('123','Not much to do on this blog','comp441'),('127','Posting on this blog is fun','comp443'),('130','Im leaving for another blog','comp440');
 
 
 --
@@ -64,10 +59,7 @@ DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  --KEY `dept_name` (`dept_name`),
-  --CONSTRAINT `instructor_ibfk_1` FOREIGN KEY (`dept_name`) REFERENCES `department` (`dept_name`) ON DELETE SET NULL,
-  --CONSTRAINT `instructor_chk_1` CHECK ((`salary` > 29000))
+  PRIMARY KEY (`id`)
 );
 --
 -- Dumping data for table `tags`
