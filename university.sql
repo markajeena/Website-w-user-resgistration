4cac0fc0d16dc8f9bce3c4e2ba4e9ce0199bb464
markajeena <markajeena@gmail.com>
2022-04-21 21:43:09 -0700

    Setup a new page called check to list all data, sql file updated

--- university.sql
+++ university.sql
@@ -74,16 +74,15 @@ INSERT INTO `tags` VALUES ('14365','NewBlog'),('15347','Friendly'),('19368','Bor
 --
 -- Table structure for table "hobby"
 --
-DROP TABLE IF EXISTS `hobby`;
 CREATE TABLE `hobby` (
   `hobbyId` int(11) NOT NULL AUTO_INCREMENT,
   `hobby` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `username` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`hobbyId`),
   KEY `username` (`username`),
-  CONSTRAINT `hobby_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL,
+  CONSTRAINT `hobby_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE SET NULL
 );
 --
---Dumping daata for table `hobby`
+-- Dumping data for table `hobby`
 --
 INSERT INTO `hobby` VALUES ('1','Soccer','comp440'),('2','Video Games', 'comp441'),('3','Streaming','comp442'),('4','Casting','comp443'),('5','Basketball','comp444');
