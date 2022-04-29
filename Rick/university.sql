f27a5d3d875a699a821bc9376b488f0fa9a382c7
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2022-04-29 13:32:32 -0700

    Changed to revert mistake and update file

--- Rick/university.sql
+++ Rick/university.sql
@@ -1,79 +1,110 @@
-<?php
+-- Before execute the file, ADD your database name here:
+-- The database name should be the same as your database of your user table from step 1
+use `phase1`;
 
-session_start();
-if(!isset($_SESSION['username'])){
+--
+-- Table structure for table `department`
+--
 
-  $_SESSION['msg'] = "Please Sign In to View";
-  header('Location: login.php');
+DROP TABLE IF EXISTS `follower`;
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `tag`;
+DROP TABLE IF EXISTS `blog`;
+DROP TABLE IF EXISTS `hobbies`;
+DROP TABLE IF EXISTS `user`;
 
-}
+CREATE TABLE `user`(
+  `userid` bigint(20) COLLATE utf8mb4_general_ci NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`userid`));
+--
+-- Dumping data for table `user`
+--
+INSERT INTO `user` VALUES ('1','comp440','pass1234', 'Jon', 'Doe', 'mysteryman@missing.org'),('2','comp440','pass1234', 'Ricardo', 'Carrillo', 'bearbear@copcop.com'),('3','comp440','pass1234','Andre', 'Obispo', 'Definitely@notme.com'),('4','comp440','pass1234', 'Rick', 'Rolled', 'GetTrolled@aol.com'),('5','comp440','pass1234','Your','Mom','CoolChick@gmail.com');
 
-if(isset($_GET['logout'])){
+--
+-- Table structure for table `blog`
+--
 
-  session_destroy();
-  unset($_SESSION['username']);
-  header('Location: login.php');
+CREATE TABLE `blog` (
+  `blogid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8mb4_general_ci NOT NULL,
+  `blog_date` DATE DEFAULT NULL,
+  `user_id` bigint(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`blogid`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userid`) ON DELETE SET NULL
+);
+--
+-- Dumping data for table `blog`
+--
+INSERT INTO `blog` VALUES ('111','Hi There','Welcome to the blog. Please enjoy your stay', '2022-05-01','1'),('112','Pleasure to be here','I hope to talk to everyone soon','2022-05-01','1'),('113','Not Good','I dont like this blog','2022-05-01','2'),('114','Rice Cakes','I like rice cakes','2022-05-03','3'),('115','This sucks','Not using this blog anymore','2022-05-04','1');
 
-}
- ?>
+CREATE TABLE `comment` (
+  `commentid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `sentiment` BOOLEAN COLLATE utf8mb4_general_ci NOT NULL,
+  `blog_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`commentid`),
+  KEY `blog_id` (`blog_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`blogid`) ON DELETE SET NULL,
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userid`) ON DELETE SET NULL
 
-<?php include("server.php") ?>
-
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title> Home Page </title>
-   </head>
-   <body>
-
-     <style>
-         body { background-color: rgba(0, 128, 0, 0.3) }
-       </style>
-
-     <?php if(isset($_SESSION['username'])) : ?>
-
-     <h3>Welcome <strong><?php echo $_SESSION['username'] ?></strong></h3>
-
-     <?php foreach ($query_user as $q) {?>
-
-       <div>
-         <a href="index.php?userid=<?php echo $q['userid']?>"><button> Follow <?php echo $q['username']?> </button></a>
-       </div>
-
-       <?php  } ?>
-
-     <div class="container mt-5">
-        <div class="text-center">
-          <a href="queries.php"><button> GO TO QUERIES </button></a>
-        </div>
-
-     <div class="container mt-5">
-        <div class="text-center">
-          <a href="create.php"><button>+ Create a new post</button></a>
-        </div>
-
-        <?php foreach ($query as $q) {?>
-          <div class="card">
-
-            <div>
-              <h3><?php echo $q['subject'] ?></h3>
-            </div>
-
-            <div class="container">
-              <?php echo $q['description'] ?>
-            </div>
-
-            <hr size = 3 color = black>
-            <a href="view.php?blogid=<?php echo $q['blogid']?>"><button> View Post </button></a>
-          </div>
+);
+INSERT INTO `comment` VALUES ('101','Hi there Nice to meet you','1' ,'111','1'),('105','Lets be Friends!','1' ,'111','2'),('123','Hiiiiiiiiiiii','1' ,'112','3'),('127','I love waffles','1' ,'113' ,'3'),('130','I dont lose','0' ,'114','1');
 
 
-        <?php  } ?>
+--
+-- Table structure for table "tag"
+--
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag` (
+  `tagid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `blog_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`tagid`),
+  KEY `blog_id` (`blog_id`),
+  CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`blogid`) ON DELETE SET NULL
+);
+--
+-- Dumping data for table `tag`
+--
+INSERT INTO `tag` VALUES ('155','NewHere', '111'),('156','WantToMeetPpl', '112'),('157','Yawn', '113'),('158','Excited, RiceCakes', '114'),('159','BuhBye', '115');
 
-      </div>
+--
+-- Table structure for table "hobies"
+--
+CREATE TABLE `hobbies` (
+  `hobbyid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `hobby` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`hobbyid`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `hobby_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`userid`) ON DELETE SET NULL
+);
+--
+-- Dumping data for table `hobbies`
+--
 
-   <?php endif ?>
+INSERT INTO `hobbies` VALUES ('50','swimming','1'),('51','bowling','2'),('53','movie','3'),('55','dancing','4'),('56','cooking','4');
 
-   </body>
- </html>
+--
+-- Table structure for table "follower"
+--
+CREATE TABLE `follower` (
+  `followerid` bigint(20) DEFAULT NULL,
+  `followingid` bigint(20) DEFAULT NULL,
+  KEY `followingid` (`followingid`),
+  CONSTRAINT `follower_ibfk_1` FOREIGN KEY (`followingid`) REFERENCES `user` (`userid`) ON DELETE SET NULL
+);
+--
+-- Dumping data for table `follower`
+--
+INSERT INTO `follower` VALUES ('2','1'),('3', '2'),('2','3'),('1','4'),('3','5');
