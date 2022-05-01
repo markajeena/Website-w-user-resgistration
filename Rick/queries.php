c617175b7802121550d26a673049da1f1f5d6dae
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2022-04-30 21:16:24 -0700

    All but the last one works!

--- Rick/queries.php
+++ Rick/queries.php
@@ -14,14 +14,11 @@
             body { background-color: rgba(0, 128, 0, 0.3) }
           </style>
 
-          <div class="container mt-5">
             <form action="queries.php" method="GET">
-
-              <p>List All Users who have at least two blogs, Where one has Tag <input type="text" name="tag_1" placeholder=" 'X' "> and One has Tag <input type="text" name="tag_2" placeholder=" 'Y' ">  <input type="submit"></p>
+            <p>List All Users who have at least two blogs, Where one has Tag <input type="text" name="tag_1" placeholder=" 'X' "> and One has Tag <input type="text" name="tag_2" placeholder=" 'Y' ">  <input type="submit"></p>
             </form>
 
-              <?php
-                  error_reporting(E_ERROR | E_PARSE);
+              <?php // querie 1
                     $x = $_REQUEST["tag_1"];
                     $y = $_REQUEST["tag_2"];
                     $need_x = true;
@@ -66,7 +63,11 @@
 
                   $lastid = $q['user_id'];
                   } ?>
-  <form action="queries.php" method="GET">
+
+
+
+
+              <form action="queries.php" method="GET">
               <p>List all the blogs of user <input type="text" name="user" placeholder=" 'X' ">, such that all the comments are positive for these blogs  <input type="submit"> </p><br>
               </form>
 
@@ -75,75 +76,98 @@
 
               $sql = "SELECT * FROM blog WHERE blogid IN (SELECT blog_id FROM comment GROUP BY blog_id HAVING COUNT(*)=SUM(sentiment))";
               $query = mysqli_query($db,$sql);
-
-              $sql = "SELECT * FROM blog WHERE blogid IN (SELECT blog_id FROM comment GROUP BY blog_id HAVING COUNT(*)=SUM(sentiment))";
-              $query = mysqli_query($db,$sql);
               ?>
+
                      <?php foreach($query as $q){
-                       $blogid = $q['blogid'];
+                            $blogid = $q['blogid'];
+                            if($q['user_id'] == $x){
                        ?>
-                        <div>Blog ID: <?php echo $q['blogid']; ?></div>
-                     <?php } ?>
+                              <div>Blog ID: <?php echo $q['blogid']; ?></div>
+                     <?php
+                            }
+
+                          } ?>
 
 
               <p>List the users who posted the most number of blogs on 5/1/2022; if there is a tie, list all the users who have a tie:</p><br>
-              <?php $sql = "SELECT user_id, COUNT(user_id) FROM blog WHERE blogid IN( SELECT blogid FROM blog WHERE blog_date = '2022-05-01' ) GROUP BY user_id HAVING COUNT(user_id) =( SELECT MAX(mycount) FROM ( SELECT user_id, COUNT(user_id) mycount FROM blog WHERE blogid IN( SELECT blogid FROM blog WHERE blog_date = '2022-05-01' ) GROUP BY user_id ) AS maxcount )";
-                     $query = mysqli_query($db,$sql); ?>
+              <?php // querie 3
+
+                    $sql = "SELECT user_id, COUNT(user_id) FROM blog WHERE blogid IN( SELECT blogid FROM blog WHERE blog_date = '2022-05-01' ) GROUP BY user_id HAVING COUNT(user_id) =( SELECT MAX(mycount) FROM ( SELECT user_id, COUNT(user_id) mycount FROM blog WHERE blogid IN( SELECT blogid FROM blog WHERE blog_date = '2022-05-01' ) GROUP BY user_id ) AS maxcount )";
+                    $query = mysqli_query($db,$sql); ?>
                      <?php foreach($query as $q){ ?>
-                        <div><a><?php echo $q['user_id']; ?></a></div>
-                        <div><a><?php echo $q['COUNT(user_id)']; ?></a></div>
+                        <div><a> User ID: <?php echo $q['user_id']; ?></a></div>
                      <?php } ?>
 
 
-              <p>List the users who are followed by both X and Y. Usernames X and Y are inputs from the user:</p><br>
+              <form action="queries.php" method="GET">
+              <p>List the users who are followed by both <input type="text" name="user_1" placeholder=" 'X' "> and <input type="text" name="user_2" placeholder=" 'Y' "> <input type="submit"> </p><br>
+              </form>
 
-              <?php
-              //this works for follower table just need to set up user input and replace the direct variable calls for this  *******this one********************************************************************and this one
-              $sql = "SELECT DISTINCT followingid FROM `follower` WHERE followingid IN (SELECT followingid FROM `follower` WHERE followerid='1') AND followingid IN (SELECT followingid FROM `follower` WHERE followerid='2');";
+              <?php // querie 4
+              $x = $_REQUEST["user_1"];
+              $y = $_REQUEST["user_2"];
+
+              $sql = "SELECT DISTINCT followingid FROM `follower` WHERE followingid IN (SELECT followingid FROM `follower` WHERE followerid='$x') AND followingid IN (SELECT followingid FROM `follower` WHERE followerid='$y');";
                      $query = mysqli_query($db,$sql); ?>
                      <?php foreach($query as $q){ ?>
-                        <div><a><?php echo $q['following']; ?></a></div>
+                        <div><a> User ID: <?php echo $q['followingid']; ?></a></div>
                      <?php } ?>
 
 
               <p>List a user pair (A, B) such that they have at least one common hobby:</p><br>
-              <?php $sql = "SELECT h1.user_id FROM hobbies AS h1, hobbies AS h2 WHERE h1.hobby=h2.hobby AND h1.user_id <> h2.user_id";
-                     $query = mysqli_query($db,$sql); ?>
-                     <?php foreach($query as $q){ ?>
-                        <div>User ID: <a><?php echo $q['user_id']; ?></a></div>
-                     <?php } ?>
+              <?php // querie 5
+                    $isA = true;
+                    $sql = "SELECT h1.user_id FROM hobbies AS h1, hobbies AS h2 WHERE h1.hobby=h2.hobby AND h1.user_id <> h2.user_id";
+                    $query = mysqli_query($db,$sql); ?>
+                     <?php foreach($query as $q){
+
+                             if($isA){
+                       ?>
+                   <a>(<?php echo $q['user_id'];
+                              $isA = false;
+                            }
+                            else{?> </a>
+
+                  <a>, <?php echo $q['user_id'];
+                              $isA = true;
+                          ?>) </a>
+                     <?php  }
+
+                      } ?>
 
 
               <p>Display all the users who never posted a blog:</p><br>
-              <?php $sql = "SELECT userid FROM user WHERE userid NOT IN (SELECT user_id FROM blog)";
-                     $query = mysqli_query($db,$sql); ?>
+              <?php // querie 6
+                    $sql = "SELECT userid FROM user WHERE userid NOT IN (SELECT user_id FROM blog)";
+                    $query = mysqli_query($db,$sql); ?>
                      <?php foreach($query as $q){ ?>
-                        <div>User ID: <a><?php echo $q['userid']; ?></a></div>
+                        <div><a> User ID: <?php echo $q['userid']; ?></a></div>
                      <?php } ?>
 
 
               <p>Display all the users who never posted a comment:</p><br>
-              <?php $sql = "SELECT userid FROM user WHERE userid NOT IN (SELECT user_id FROM comment)";
-                     $query = mysqli_query($db,$sql); ?>
+              <?php // querie 7
+                    $sql = "SELECT userid FROM user WHERE userid NOT IN (SELECT user_id FROM comment)";
+                    $query = mysqli_query($db,$sql); ?>
                      <?php foreach($query as $q){ ?>
-                        <div>User ID: <a><?php echo $q['userid']; ?></a></div>
+                        <div><a> User ID: <?php echo $q['userid']; ?></a></div>
                      <?php } ?>
 
 
               <p>Display all the users who posted some comments, but each of them is negative:</p><br>
-              <?php
-              $sql = "SELECT userid FROM user WHERE userid IN (SELECT user_id FROM comment GROUP BY user_id HAVING 0=SUM(sentiment))";
-              $query = mysqli_query($db,$sql);
+              <?php // querie 8
+                    $sql = "SELECT userid FROM user WHERE userid IN (SELECT user_id FROM comment GROUP BY user_id HAVING 0=SUM(sentiment))";
+                    $query = mysqli_query($db,$sql);
               ?>
                      <?php foreach($query as $q){ ?>
-                        <div><a> <?php echo $q['user_id']; ?></a></div>
+                        <div><a> User ID: <?php echo $q['userid']; ?></a></div>
                      <?php } ?>
 
 
               <p>Display those users such that all the blogs they posted so far never received any negative comments:</p><br>
-              <?php
-              $sql = "SELECT blog_id FROM comment GROUP BY blog_id HAVING COUNT(*)=SUM(sentiment)";
-              $query = mysqli_query($db,$sql); ?>
+              <?php // querie 9
+                    $sql = "SELECT blog_id FROM comment GROUP BY blog_id HAVING COUNT(*)=SUM(sentiment)";
+                    $query = mysqli_query($db,$sql); ?>
 
                      <?php foreach($query as $q){
                             $blogid = $q['blog_id'];
